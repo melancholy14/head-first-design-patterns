69def5a34cfda278c39780ae42ed8c1b5991623b
Misol Goh <melancholy8914@gmail.com>
2023-10-22 20:41:44 +0900

    UPDATE: 동적으로 행동 지정하기 예시 코드 추가

--- Chapter01/codes/Duck.ts
+++ Chapter01/codes/Duck.ts
@@ -23,6 +23,15 @@ class Duck {
     swim() {
         console.log('모든 오리는 물에 뜹니다. 가짜 오리도 뜨죠.');
     }
+
+    // 동적으로 행동 지정할 수 있도록 세터 메소드 생성
+    setFlyBehavior(fb: FlyBehavior) {
+        this.flyBehavior = fb;
+    }
+
+    setQuackBehavior(qb: QuackBehavior) {
+        this.quackBehavior = qb;
+    }
 }
 
 export default Duck;
--- Chapter01/codes/MiniDuckSimulator.ts
+++ Chapter01/codes/MiniDuckSimulator.ts
@@ -1,10 +1,21 @@
+import FlyRocketPowered from "./FlyRocketPowered";
 import MallardDuck from "./MallardDuck";
+import ModelDuck from "./ModelDuck";
 
 function MiniDuckSimulator() {
     const mallard = new MallardDuck();
 
+    // mallard.display();
     mallard.performQuack();
     mallard.performFly();
+
+    const model = new ModelDuck();
+    
+    // model.display();
+    model.performFly();
+    // 동적으로 로켓 추진 기능을 부여
+    model.setFlyBehavior(new FlyRocketPowered());
+    model.performFly();
 }
 
 MiniDuckSimulator();
