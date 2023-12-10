d23e93ffeddfa2644c934a6f2624b3ddcfc44792
고미솔 <misol.goh@kurlycorp.com>
2023-12-10 15:56:34 +0900

    UPDATE: 싱글턴 패턴 예시 코드 추가

---
+++ Chapter05/codes/ChocolateBoilerWithMultithreading.ts
@@ -0,0 +1,49 @@
+class ChocolateBoilerWithMultithreading {
+    private empty: boolean;
+    private boiled: boolean;
+
+    // 타입스크립트에는 synchronized, volatile 키워드가 없기 때문에 처음부터 생성하는 방법으로 해결 가능합니다. 
+    private static uniqueInstance = new ChocolateBoilerWithMultithreading();
+
+    private constructor() {
+        this.empty = true;
+        this.boiled = false;
+    }
+
+    static getInstance() {
+        return this.uniqueInstance;
+    }
+
+    isEmpty() {
+        return this.empty;
+    }
+
+    isBoiled() {
+        return this.boiled;
+    }
+
+    fill() {
+        if (this.isEmpty()) {
+            this.empty = false;
+            this.boiled = false;
+
+            console.log('보일러에 우유와 초콜릿을 혼합한 재료를 넣음');
+        }
+    }
+
+    drain() {
+        if(!this.isEmpty() && this.isBoiled()) {
+            console.log('끓인 재료를 다음 단계로 넘김');
+
+            this.empty = true;
+        }
+    }
+
+    boil() {
+        if(!this.isEmpty() && !this.isBoiled()) {
+            console.log('재료를 끓임');
+
+            this.boiled = true;
+        }
+    }
+}
